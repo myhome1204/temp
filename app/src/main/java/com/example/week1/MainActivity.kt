8d37690b3fede0da714439f7a5ff600e35b33cb5
choijunwon <myhome120411@gmail.com>
2024-05-15 15:49:53 +0900

    .

--- app/src/main/java/com/example/week1/MainActivity.kt
+++ app/src/main/java/com/example/week1/MainActivity.kt
@@ -56,7 +56,8 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.lookFragment -> {
-                    supportFragmentManager.beginTransaction()                        .replace(R.id.main_frm, LookFragment())
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, LookFragment())
                         .commitAllowingStateLoss()
                     return@setOnItemSelectedListener true
                 }
